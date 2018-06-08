c44542295d095a7979ebb645370d67a0d4bbb16a
KeithBeatTheBeast <keithgmills34@gmail.com>
2018-06-08 14:05:05 -0600

    Passed CLTO test

--- src/EFM32_throwI2CErrors.h
+++ src/EFM32_throwI2CErrors.h
@@ -25,13 +25,13 @@ void vThrowI2CErrors(void *iDontCare) {
 //		I2C1->IFS |= I2C_IFS_ARBLOST;
 //		vTaskDelay(portTICK_PERIOD_MS * delayMult);
 
-		puts("THROW BUSERR");
-		I2C1->IFS |= I2C_IFS_BUSERR;
-		vTaskDelay(portTICK_PERIOD_MS * delayMult);
-
-//		puts("THROW CLTO");
-//		I2C1->IFS |= I2C_IFS_CLTO;
+//		puts("THROW BUSERR");
+//		I2C1->IFS |= I2C_IFS_BUSERR;
 //		vTaskDelay(portTICK_PERIOD_MS * delayMult);
+
+		puts("THROW CLTO");
+		I2C1->IFS |= I2C_IFS_CLTO;
+		vTaskDelay(portTICK_PERIOD_MS * delayMult);
 
 //		puts("THROW BITO");
 //		I2C1->IFS |= I2C_IFS_BITO;
